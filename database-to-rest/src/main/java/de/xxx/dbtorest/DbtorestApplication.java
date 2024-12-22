a7505bcae41108f16cddab8cb4e97734c8f31075
Angular2guy <angular2guy@gmail.com>
2024-12-22 12:35:37 +0100

    feat: init

---
+++ database-to-rest/src/main/java/de/xxx/dbtorest/DbtorestApplication.java
@@ -0,0 +1,13 @@
+package de.xxx.dbtorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbtorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbtorestApplication.class, args);
+	}
+
+}
