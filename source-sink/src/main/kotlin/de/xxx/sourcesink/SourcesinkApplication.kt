da2652f423a6d0e0e6f9fb036f7e9c98c8a98b13
Angular2guy <angular2guy@gmail.com>
2024-12-24 15:28:50 +0100

    feat: add source sink

---
+++ source-sink/src/main/kotlin/de/xxx/sourcesink/SourcesinkApplication.kt
@@ -0,0 +1,11 @@
+package de.xxx.sourcesink
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SourcesinkApplication
+
+fun main(args: Array<String>) {
+	runApplication<SourcesinkApplication>(*args)
+}
