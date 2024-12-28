1f3e8fbbe95d72ab3f37774db67bf7c7f8544b2b
Angular2guy <angular2guy@gmail.com>
2024-12-28 16:55:19 +0100

    feat: use jpa

--- runPostgresql.sh
+++ runPostgresql.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker pull postgres:latest
-docker run --name datastream-postgres -e POSTGRES_PASSWORD=sven1 -e POSTGRES_USER=sven1 -e POSTGRES_DB=aidoclibchat -p 5432:5432 -d postgres
+docker run --name datastream-postgres -e POSTGRES_PASSWORD=sven1 -e POSTGRES_USER=sven1 -e POSTGRES_DB=datastream -p 5432:5432 -d postgres
 
 # docker start datastream-postgres
 # docker stop datastream-postgres
