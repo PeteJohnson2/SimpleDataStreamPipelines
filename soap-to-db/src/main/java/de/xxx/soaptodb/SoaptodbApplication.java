22c82d659986066725296ae806e62311bcac5aea
Angular2guy <angular2guy@gmail.com>
2025-01-04 11:13:51 +0100

    feat: add stream

---
+++ soap-to-db/src/main/java/de/xxx/soaptodb/SoaptodbApplication.java
@@ -0,0 +1,13 @@
+package de.xxx.soaptodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaptodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaptodbApplication.class, args);
+	}
+
+}
---
+++ soap-to-db/src/test/java/de/xxx/soaptodb/SoaptodbApplicationTests.java
@@ -0,0 +1,13 @@
+package de.xxx.soaptodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaptodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
