eb0d632f84e40be4137e0b4801b8f29d0cb1f9d6
Angular2guy <angular2guy@gmail.com>
2025-01-04 11:56:23 +0100

    feat: ws first step

---
+++ soap-to-db/src/main/java/de/xxx/soaptodb/source/CountryRepository.java
@@ -0,0 +1,47 @@
+package de.xxx.soaptodb.source;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+import io.spring.guides.gs_producing_web_service.Country;
+import io.spring.guides.gs_producing_web_service.WsCurrency;
+import org.springframework.util.Assert;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class CountryRepository {
+    private static final Map<String, Country> countries = new HashMap<>();
+
+    @PostConstruct
+    public void initData() {
+        Country spain = new Country();
+        spain.setName("Spain");
+        spain.setCapital("Madrid");
+        spain.setCurrency(WsCurrency.EUR);
+        spain.setPopulation(46704314);
+
+        countries.put(spain.getName(), spain);
+
+        Country poland = new Country();
+        poland.setName("Poland");
+        poland.setCapital("Warsaw");
+        poland.setCurrency(WsCurrency.PLN);
+        poland.setPopulation(38186860);
+
+        countries.put(poland.getName(), poland);
+
+        Country uk = new Country();
+        uk.setName("United Kingdom");
+        uk.setCapital("London");
+        uk.setCurrency(WsCurrency.GBP);
+        uk.setPopulation(63705000);
+
+        countries.put(uk.getName(), uk);
+    }
+
+    public Country findCountry(String name) {
+        Assert.notNull(name, "The country's name must not be null");
+        return countries.get(name);
+    }
+}
