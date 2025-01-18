e101e307ab12e1a8b77b8939b2d3c0f059333d1a
Angular2guy <angular2guy@gmail.com>
2025-01-18 10:21:36 +0100

    feat: add eventtofile

---
+++ event-to-file/src/main/java/de/xxx/eventtofile/kafka/KafkaProducer.java
@@ -0,0 +1,51 @@
+/**
+ *    Copyright 2023 Sven Loesekann
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+package de.xxx.eventtofile.kafka;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.xxx.eventtofile.model.FlightDto;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class KafkaProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+    private final AdminClient adminClient;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate, AdminClient adminClient, ObjectMapper objectMapper) {
+        this.adminClient = adminClient;
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public void sendCountryMsg(FlightDto flightDto) {
+        try {
+            String msg = this.objectMapper.writeValueAsString(flightDto);
+            CompletableFuture<SendResult<String, String>> listenableFuture = this.kafkaTemplate
+                    .send(KafkaConfig.FLIGHT_TOPIC, flightDto.id().toString(), msg);
+            listenableFuture.get(2, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            throw new RuntimeException("Send Country failed.", e);
+        }
+        LOGGER.info("send CountryDto msg: {}", flightDto.toString());
+    }
+}
---
+++ event-to-file/src/test/java/de/xxx/eventtofile/EventtofileApplicationTests.java
@@ -0,0 +1,13 @@
+package de.xxx.eventtofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventtofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
