b65a77188da5b8134f89f0745c868391bf5bb368
Angular2guy <angular2guy@gmail.com>
2025-02-03 16:19:22 +0100

    feat: next dockerfile

---
+++ database-to-rest/buildDocker.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#./mvnw clean install -Ddocker=true -Dnpm.test.script=test-chromium
+../mvnw clean install
+docker build -t angular2guy/sourcesink:latest --build-arg JAR_FILE=eventtofile-0.0.1-SNAPSHOT.jar --no-cache .
+docker run -p 8080:8080 --memory="384m" --network="host" angular2guy/sourcesink:latest
