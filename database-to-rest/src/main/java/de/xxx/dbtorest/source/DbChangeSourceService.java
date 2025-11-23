d317523a03559b27f7808dc7c8d3dfc012d4fb6f
Angular2guy <angular2guy@gmail.com>
2025-11-23 16:36:51 +0100

    fix: empty debezium values

--- database-to-rest/src/main/java/de/xxx/dbtorest/source/DbChangeSourceService.java
+++ database-to-rest/src/main/java/de/xxx/dbtorest/source/DbChangeSourceService.java
@@ -1,14 +1,14 @@
 /**
- *    Copyright 2023 Sven Loesekann
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ * Copyright 2023 Sven Loesekann
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package de.xxx.dbtorest.source;
 
@@ -18,6 +18,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.function.Predicate;
 
 @Service
 @Transactional
@@ -31,6 +34,7 @@ public class DbChangeSourceService {
 
     public void sendChange(DbChangeDto dbChangeDto) {
         //LOGGER.info(dbChangeDto.toString());
-        this.kafkaProducer.sendDbChangeMsg(dbChangeDto);
+        Optional.ofNullable(dbChangeDto.value()).stream().filter(Predicate.not(String::isBlank)).findFirst()
+                .ifPresent(x -> this.kafkaProducer.sendDbChangeMsg(dbChangeDto));
     }
 }
